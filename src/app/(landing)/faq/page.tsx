433c3768216285a1a9c0898a597217a22a993d1c
aanonpro <anonphin.pro@gmail.com>
2025-05-17 16:47:49 +0700

    new update

--- src/app/(landing)/faq/page.tsx
+++ src/app/(landing)/faq/page.tsx
@@ -1,5 +1,9 @@
 export default function Page(){
     return(
+        <>
         <h2 className="text-center text-2xl">welcome to faq page</h2>
+        <h2 className="text-center text-2xl">second page</h2>
+        </>
+        
     )
 }
