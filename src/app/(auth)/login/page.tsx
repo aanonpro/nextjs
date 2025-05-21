db1361585721e1fd678f25450d6257acd59b0534
aanonpro <anonphin.pro@gmail.com>
2025-05-21 21:37:24 +0700

    update form login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,11 @@
-export default function Page(){
+import { LoginForm } from "@/components/login-form";
+
+export default function Page() {
     return (
-        <h2 className="text-center text-2xl">Welcome login page</h2>
+        <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <LoginForm />
+            </div>
+        </div>
     )
 }
